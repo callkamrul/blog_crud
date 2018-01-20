4b6531becaadc86a5f6bd27d758f51a6f2a95eb3
Kamrul Hasan <kamrul.hasan@technovista.com.bd>
2018-01-20 16:16:11 +0600

    basic seup for blog crud

---
+++ app/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<!-- The HTML template for our component -->
+	<div id="app">
+		
+	</div>
+</template>
+
+<script>
+	// the Javascript for our component
+	// We will export a Vue component options object here
+	
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path')
+var webpack = require('webpack')
+//const ElectronPackager = require("webpack-electron-packager");
+
+module.exports = {
+	entry:'./app/main.js',
+	output:{
+		path:path.resolve(__dirname, './dist'),
+		filename:'build.js'
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.vue$/,
+				loader:'vue'
+			},
+			{
+				test:/\.js$/,
+				loader:'babel',
+				exclude:/node_modules/
+			}
+		]
+	},
+	babel:{
+		"presets":["es2015"],
+		"plugins":["transform-runtime"]
+	},
+	plugins:[
+		new webpack.ExternalsPlugin('commonjs', [
+			'electron'
+		])
+		//
+		//new ElectronPackager({
+		//	dir: "D:/source/book_app",
+		//	arch: "x64",
+		//	platform: "win32",
+		// })
+	],
+	resolve:{
+		alias:{
+			'vue$': 'vue/dist/vue.js'
+		}
+	}
+}
